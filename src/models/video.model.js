ff82ac38d07d21ec94a76fb1ffe620e047e8b34d
Tanaytrm <tanaytechnologies@gmail.com>
2024-05-21 02:22:11 +0530

    token ki baat

---
+++ src/models/video.model.js
@@ -0,0 +1,72 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema (
+
+{
+
+videoFile:{
+    type: string, // cloudenary url
+    required: true
+
+},
+
+thumbnail:{
+    type: string, 
+    required: true
+
+},
+
+title:{
+    type: string, 
+    required: true
+
+},
+
+description:{
+    type: string, 
+    required: true
+
+},
+
+duration:{
+    type: Number, // cloudenary url
+    required: true
+
+},
+
+
+views:{
+    type: Number, 
+    default: 0
+
+},
+
+isPublished:{
+    type: Boolean, 
+    default: true
+
+},
+
+owner:{
+    type: Schema.Types.ObjectId, 
+    ref: "User"
+
+},
+
+
+
+
+
+
+
+},
+{
+timestamps: true
+}
+
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Video", videoSchema)
